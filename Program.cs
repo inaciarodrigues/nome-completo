b100984580cebd04217eede62a48676018517752
inaciarodrigues <soterinacia688@gmail.com>
2025-03-31 09:38:46 -0300

    código do exercício

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿string nome;
+string sobrenome;
+
+Console.Write("Digite o primeiro nome: ");
+nome = Console.ReadLine()!;
+
+Console.Write("Digite o sobrenome: ");
+sobrenome = Console.ReadLine()!;
+
+Console.WriteLine($"nome completo: {nome} {sobrenome}"); 
+Console.WriteLine($"nome de catálogo: {sobrenome.ToUpper()}, {nome}");
